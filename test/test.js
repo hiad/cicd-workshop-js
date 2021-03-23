0db2b0c1cca471d2d4b570d13d755b4e5517bea0
Hector Alcazar <halcaza@US-C02DKA1XMD6M>
2021-03-23 11:36:53 -0500

    chore: Adding config orbs

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ var request = require("request"),
     base_url = "http://localhost:5000/";
 
 describe("Welcome to CI/CD Server", function() {
-
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
